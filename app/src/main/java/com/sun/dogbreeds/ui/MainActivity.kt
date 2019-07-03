73ffc68d09c915eae10698a894f93e4a7a6f9889
tr4n <tr4n01@gmail.com>
2019-07-03 10:09:56 +0700

    task#14301 Init bases

--- app/src/main/java/com/sun/dogbreeds/ui/MainActivity.kt
+++ app/src/main/java/com/sun/dogbreeds/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sun.dogbreeds
+package com.sun.dogbreeds.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sun.dogbreeds.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/sun/dogbreeds/ui/base/BaseFragment.kt
@@ -0,0 +1,29 @@
+package com.sun.dogbreeds.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
+
+    protected lateinit var viewDataBinding: VB
+    protected abstract val layoutResource: Int
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        viewDataBinding = DataBindingUtil.inflate(inflater, layoutResource, container, false) as VB
+        return viewDataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initComponents()
+    }
+
+    protected abstract fun initComponents()
+}
